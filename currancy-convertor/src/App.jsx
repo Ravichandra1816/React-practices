1a78eeec6fa34f01a8a0a786159612455bed481c
Ravichandra86 <ravinew711@gmail.com>
2025-04-05 11:09:39 +0530

    currency-convertor setup will be done

---
+++ currancy-convertor/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold bg-amber-600 text-center py-8">
+        Currancy convertor
+      </h1>
+    </div>
+  );
+};
+
+export default App;
