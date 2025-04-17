0c056df7872fa81d00d75a066cc68b0465b4d764
Ravichandra86 <ravinew711@gmail.com>
2025-04-17 11:01:54 +0530

    todo2 setup done

--- dark-light/src/App.jsx
+++ dark-light/src/App.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { ThemeProvider } from "./Contex/Thems";
+import ThemeBtn from "./Components/ThemeBtn";
+import Card from "./Components/Card";
 
 const App = () => {
-  return (
-    <div className="flex flex-wrap min-h-screen items-center">
-      <div className="w-full">
-        <div className="w-full max-w-sm mx-auto flex justify-end mb-4"></div>
+  const [themeMode, setThemeMode] = useState("light");
+  const darkTheme = () => {
+    setThemeMode("dark");
+  };
+  const lightTheme = () => {
+    setThemeMode("light");
+  };
 
-        <div className="w-full max-w-sm mx-auto"></div>
+  useEffect(() => {
+    document.querySelector("html").classList.remove("dark", "light");
+    document.querySelector("html").classList.add("themeMode");
+  }, [themeMode]);
+  return (
+    <ThemeProvider value={{ lightTheme, darkTheme, themeMode }}>
+      <div className="flex flex-wrap min-h-screen items-center">
+        <div className="w-full">
+          <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+            <ThemeBtn />
+          </div>
+
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   );
 };
 
---
+++ dark-light/src/Contex/Thems.js
@@ -0,0 +1,13 @@
+import { createContext, useContext } from "react";
+
+export const ThemeContex = createContext({
+  themeMode: "light",
+  darkTheme: () => {},
+  lightTheme: () => {},
+});
+
+export const ThemeProvider = ThemeContex.Provider;
+
+export default function useTheme() {
+  return useContext(ThemeContex);
+}
